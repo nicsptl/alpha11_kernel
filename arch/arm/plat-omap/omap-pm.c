bf2e0218ce5e2bcea92a4d244acb40a220ccbf7b
Alfredo <alfredomonclus@gmail.com>
2012-06-17 22:20:39 -0300

    PM: always respect cpufreq policy limits, part 2: DSP
    Change-Id: I84605aab406f1281de9db5e40c45422f811d67b0

--- arch/arm/plat-omap/omap-pm.c
+++ arch/arm/plat-omap/omap-pm.c
@@ -590,10 +590,47 @@ void omap_pm_dsp_set_min_opp(u8 opp_id)
 	 * if it is higher than the current OPP clock rate.
 	 *
 	 */
-	if (dsp_req_id > mpu_req_id)
+	if (dsp_req_id > mpu_req_id){
+        /*
+        * Ensure that this request is not conflicting with cpufreq
+        * constraints. If that is the case, cpufreq wins.
+        */
+        struct cpufreq_policy policy;
+
+        cpufreq_get_policy(&policy, 0);
+
+        while (mpu_freq_table[dsp_req_id].frequency < policy.min && dsp_req_id < ft_count - 1) {
+        dsp_req_id = dsp_req_id + 1;
+        }
+        while (mpu_freq_table[dsp_req_id].frequency > policy.max && dsp_req_id > 0) {
+        dsp_req_id = dsp_req_id - 1;
+        }
+        /*
+        * cpufreq check end
+        */
+
 		selopp = dsp_req_id;
-	else
+	} else {
+		/*
+        * Ensure that this request is not conflicting with cpufreq
+        * constraints. If that is the case, cpufreq wins.
+        */
+        struct cpufreq_policy policy;
+
+        cpufreq_get_policy(&policy, 0);
+
+        while (mpu_freq_table[mpu_req_id].frequency < policy.min && mpu_req_id < ft_count - 1) {
+        mpu_req_id = mpu_req_id + 1;
+        }
+        while (mpu_freq_table[mpu_req_id].frequency > policy.max && mpu_req_id > 0) {
+        mpu_req_id = mpu_req_id - 1;
+        }
+        /*
+        * cpufreq check end
+        */
+        
 		selopp = mpu_req_id;
+	}
 
 	/* Is a change requested? */
 	if (currspeed == dsp_freq_table[selopp].frequency)
